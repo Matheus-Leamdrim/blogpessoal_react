673c9864ab030ab2b9ef8ac470f7b9f4370e3d17
Matheus <matheusalves.art@outlook.com>
2024-08-21 12:10:33 -0300

    Componente Home - CSS

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Home from "./assets/pages/home/home"
+
+
+function App() {
+  
+
+  return (
+    <><h1>titulo</h1>
+    <Home /></>
+  )
+}
+
+export default App
---
+++ src/assets/pages/home/home.tsx
@@ -0,0 +1,34 @@
+
+function Home() {
+    return (
+        <div style={{
+            width: "100vw",
+            display: "flex",
+            justifyContent: "center",
+           
+        }}>
+            <div>
+                <div style={{
+                    width: "80vw",
+                    display: "flex",
+                    flexDirection: "column",
+                    alignContent: "center",
+                }}>
+                    <h2>Seja bem vinde!</h2>
+                    <p>Expresse aqui seus pensamentos e opiniões</p>
+                </div>
+                <div style={{
+                    width: "100vw",
+                    display: "flex",
+                    justifyContent: "center",
+                   
+                }}>
+                <img src="https://i.imgur.com/VpwApCU.png" alt="Imagem da Página Home" width="400px">
+                    </img>
+                </div>
+            </div>
+      </div>
+    )
+  }
+   
+  export default Home
