46b6c2419e471cd9360b2a70f85cb39b64f83fc5
Matheus <matheusalves.art@outlook.com>
2024-08-23 16:23:25 -0300

    "Components"

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./assets/pages/home/home"
+import Footer from "./components/footer/Footer";
+import Navbar from "./components/navbar/Navbar"
+import Cadastro from "./assets/pages/cadastro/Cadastro";
+
 
 
 function App() {
   
 
   return (
-    <><h1>titulo</h1>
-    <Home /></>
+    <>
+    <BrowserRouter>
+    <Navbar/>
+    <div className="min-h-[80vh]">
+    <Routes>
+    <Route path ="/" element={<Cadastro/>}/>
+    <Route path ="/home" element={<Home/>}/>
+    </Routes>
+    </div>
+    <Footer/>
+    </BrowserRouter>
+    </>
   )
 }
 
-export default App
+export default App;
---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,21 @@
+import { FacebookLogo, InstagramLogo, LinkedinLogo } from "@phosphor-icons/react"
+
+let data = new Date().getFullYear()
+
+function Footer() {
+  return (
+        <div className="flex justify-center bg-gray-900 text-white">
+          <div className="container flex flex-col items-center py-3">
+            <p className='text-xl font-bold'>Blog Pessoal - Matheus Alves | Copyright: </p>
+            <p className='text-lg'>Acesse as Redes Sociais!</p>
+            <div className='flex gap-6'>
+              <LinkedinLogo size={50} weight='light' />
+              <InstagramLogo size={50} weight='light' />
+              <FacebookLogo size={50} weight='light' />
+            </div>
+          </div>
+        </div>
+  )
+}
+
+export default Footer
